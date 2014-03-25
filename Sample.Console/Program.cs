93864c680f6794fd987bc2167398a2ab070476f4
drexyia <drexyia@fastmail.fm>
2014-03-25 10:53:32 +0000

    Initial commit contains code to retrieve name value pairs from a Yaml App.Config using YamlDotNet

---
+++ Sample.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+using msConfig = System.Configuration;
+using drexyiaConfig = Drexyia.Yaml.Configuration;
+
+namespace ConsoleUI {
+    class Program {
+        static void Main(string[] args) {
+
+            var msValue1 = msConfig.ConfigurationManager.AppSettings["key1"];
+            var drexyiaValue1 = drexyiaConfig.ConfigurationManager.AppSettings["key1"];
+
+            Debug.Assert(msValue1 == drexyiaValue1);
+
+            msValue1 = msConfig.ConfigurationManager.AppSettings["key2"];
+            drexyiaValue1 = drexyiaConfig.ConfigurationManager.AppSettings["key2"];
+
+            Debug.Assert(msValue1 == drexyiaValue1);
+
+            msValue1 = msConfig.ConfigurationManager.AppSettings[0];
+            drexyiaValue1 = drexyiaConfig.ConfigurationManager.AppSettings[0];
+
+            Debug.Assert(msValue1 == drexyiaValue1);
+
+            msValue1 = msConfig.ConfigurationManager.AppSettings[1];
+            drexyiaValue1 = drexyiaConfig.ConfigurationManager.AppSettings[1];
+
+            Debug.Assert(msValue1 == drexyiaValue1);
+
+        }
+    }
+}
---
+++ Yaml.Configuration/ConfigurationManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+using YamlDotNet.RepresentationModel;
+
+namespace Drexyia.Yaml.Configuration {
+    public static class ConfigurationManager {
+
+        public static NameValueCollection AppSettings {
+            get {
+
+                var nameValues = new NameValueCollection();
+
+                using (TextReader reader = File.OpenText(@"App.config.yaml")) {
+                    var yaml = new YamlStream();
+                    yaml.Load(reader);
+
+                    var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+
+                    foreach (var entry in mapping.Children) {
+                        var rootNode = (YamlScalarNode)entry.Key;
+                        if (rootNode.Value == "app-settings") {
+
+                            var appSettings = (YamlMappingNode)entry.Value;
+                            foreach (var appSetting in appSettings.Children) {
+
+                                var nodeKey = (YamlScalarNode)appSetting.Key;
+                                var nodeValue = (YamlScalarNode)appSetting.Value;
+
+                                nameValues[nodeKey.Value] = nodeValue.Value;
+                            }
+                        }
+                    }
+                }
+
+                return nameValues;
+
+            }
+        }
+    }
+}
